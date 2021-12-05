844f4f4ca50e6cc03587705de54db25f1c3ee102
nhutpro <tnhut803@gmail.com>
2021-12-05 07:11:12 +0700

    add app shell

---
+++ MobileApp/MobileApp/Views/DashBoardPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MobileApp.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DashBoardPage : ContentPage
+    {
+        public DashBoardPage()
+        {
+            InitializeComponent();
+            List<Item> item = new List<Item>();
+            item.Add(new Item { Id = "Thuốc Trừ Sâu", Description = "ahihi", Text = "450000" });
+            LskItems.ItemsSource = item;
+        }
+    }
+}
