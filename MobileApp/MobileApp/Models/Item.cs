d625fb1b2e1153e771faa7e9b3c8520c1284e169
nhutpro <tnhut803@gmail.com>
2021-12-09 16:17:22 +0700

    ok

--- MobileApp/MobileApp/Models/Item.cs
+++ MobileApp/MobileApp/Models/Item.cs
@@ -5,7 +5,10 @@ namespace MobileApp.Models
     public class Item
     {
         public string Id { get; set; }
-        public string Text { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        public string Text {get; set;}
     }
 }
--- MobileApp/MobileApp/Views/DashBoardPage.xaml.cs
+++ MobileApp/MobileApp/Views/DashBoardPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using MobileApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Globalization;
+using System.Net.Http;
 
 namespace MobileApp.Views
 {
@@ -15,9 +17,24 @@ namespace MobileApp.Views
         public DashBoardPage()
         {
             InitializeComponent();
-            List<Item> item = new List<Item>();
-            item.Add(new Item { Id = "Thuốc Trừ Sâu", Description = "ahihi", Text = "450000" });
+            double ad = 300000;
+            List < Item > item = new List<Item>();
+            for(var i = 0; i< 10; i++)
+            {
+                item.Add(new Item { Id = "1", Description = "Thuốc trừ sâu", Name = "Thuốc trừ sâu", Image = "anhthuoc.jpg", Price = 1000000 });
+            }    
+               
             LskItems.ItemsSource = item;
+          
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            HttpClient httpClient = new HttpClient();
+            var result = await httpClient.GetStringAsync("http://172.19.48.1/WebApplication2/api/ServiceController/HelloAPI");
+            button.Text = result;
+            
+
         }
     }
 }
